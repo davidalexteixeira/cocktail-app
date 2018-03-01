d3dd4552bff472d7560b6a64b02e5c95ae13428b
Danielle.Leblanc-Cyr <danielleleb12@gmail.com>
2018-03-01 21:41:21 +0100

    added to results component.ts

--- src/app/services/drink.service.ts
+++ src/app/services/drink.service.ts
@@ -1,8 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Subject } from 'rxjs/Subject';
+
 
 @Injectable()
 export class DrinkService {
+  private drink: any;
+  private drinkChange: Subject<any> = new Subject();
 
-  constructor() { }
+
+  API_URL = 'http://localhost:3000/';
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  private setDrink(drink?: any) {
+    this.drink = drink;
+    this.drinkChange.next(drink);
+    return drink;
+  }
+  
+  getDrinkById(drinkId) {
+    const options = {
+      withCredentials: true
+    };
+    return this.httpClient.get(`${this.API_URL}/${drinkId}`, options)
+    .toPromise()
+    // .then((data) => console.log(data))
+    .then((data) => this.setDrink(data))
+  }
 
 }
